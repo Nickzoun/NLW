56e1480463d73a5debbd62f038ae359271e6d894
Nicolas Vieira <macneckezs@gmail.com>
2020-06-05 15:11:30 -0300

    site completo sem back-end
    completei o create-point.html
    criei e embelezei o search-result.html
    e fiz os botão funcionar

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -19,11 +19,13 @@ function getCities(event) {
     stateInput.value = event.target.options[indexOfSelectedState].text
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
     fetch(url)
     .then(res => res.json())
     .then(cities => {
         for (const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -33,3 +35,31 @@ function getCities(event) {
 document
     .querySelector("select[name=state]")
     .addEventListener("change", getCities)
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelecteditem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selecteditems = []
+
+function handleSelecteditem(event) {
+    const itemLi = event.target
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+    const alreadySelected = selecteditems.findIndex(item => {
+        const itemFound = item == itemId  //tbm pode ser item => item == itemId
+        return itemFound
+    })
+    if(alreadySelected >= 0) {
+        const filteredItems = selecteditems.filter(item => item != itemId
+        )
+        selecteditems = filteredItems
+    } else {
+        selecteditems.push(itemId)
+    }
+    collectedItems.value = selecteditems
+}
